126e10b32269917172081c40d4dbfb06fe94c752
navshar1999 <naveena.sharma@gmail.com>
2022-02-21 12:55:13 -0600

    dataframe creation

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
-import urllib
 import requests
+import pandas as pd
+import numpy as np
 
 header = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'}
 
@@ -17,18 +18,33 @@ soup = BeautifulSoup(html,features="html.parser")
 #retrieve restaurant names
 
 def get_open_restaurants():
+    names = []
     for entry in soup.findAll('span',{'class':'rest-row-name-text'}):
-        name = entry.get_text()
-        print(name)
+        names.append(entry.get_text())
+    return names 
+        
 
 def get_open_price_points():
+    prices = []
     for entry in soup.findAll('i',{'class':'pricing--the-price'}):
-        price = entry.get_text()
-        print(price.count('$'))
+        prices.append(entry.get_text().count('$'))
+    return prices 
+    
     
 def get_open_locations(): 
+    locations = []
     for entry in soup.findAll('span',{'class':'rest-row-meta--location rest-row-meta-text sfx1388addContent'}):
-        location = entry.get_text()
-        print(location)
+        locations.append(entry.get_text())
+    return locations
 
-get_open_locations()
+#manipulating data
+
+names = get_open_restaurants()
+prices = get_open_price_points()
+locations = get_open_locations()
+
+data = np.array((names,prices,locations)).T
+df = pd.DataFrame(data, columns=["name","price","location"])
+
+
+
