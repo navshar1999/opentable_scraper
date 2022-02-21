449da94c7a3e7da71e46e471b632f13783fce910
navshar1999 <naveena.sharma@gmail.com>
2022-02-21 13:05:16 -0600

    rearranged

--- scraper.py
+++ scraper.py
@@ -37,15 +37,5 @@ def get_open_locations():
         locations.append(entry.get_text())
     return locations
 
-#manipulating data
-
-names = get_open_restaurants()
-prices = get_open_price_points()
-locations = get_open_locations()
-
-data = np.array((names,prices,locations)).T
-df = pd.DataFrame(data, columns=["name","price","location"])
-
-
 
 
